c06d89a647190438ed94ac4e2ef7c8140d55f0f3
siddharth agarwal <siddharth.a@outlook.com>
2021-01-10 15:46:05 -0500

    bump definition-manifest to miniconda ver

--- test-project/hello.py
+++ test-project/hello.py
@@ -3,4 +3,24 @@
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
 
-print('Hello, remote world!')
+#%%
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("test-project/plot.png")
+plt.show()
+
+print('Open test-project/plot.png to see the result!')
+#%%
